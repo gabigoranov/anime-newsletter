533b68b93def45b512911df6b65810f4640f64d6
gabigoranov <gabigoranov@abv.bg>
2026-06-07 09:41:24 -0400

    Signup popup

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,17 +1,10 @@
-import Features from "./components/landing/Features";
-import Footer from "./components/landing/Footer";
-import Hero from "./components/landing/Hero";
-import Navbar from "./components/landing/Navbar";
+import Landing from "./pages/Landing";
+import { ModalProvider } from "./providers/ModalProvider";
 
 export default function App() {
   return (
-    <div className="bg-background text-on-background min-h-screen flex flex-col selection:bg-primary-container selection:text-on-primary-container font-body-md">
-      <Navbar />
-      <main className="grow">
-        <Hero />
-        <Features />
-      </main>
-      <Footer />
-    </div>
+    <ModalProvider>
+      <Landing />
+    </ModalProvider>
   );
 }
